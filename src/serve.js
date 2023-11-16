3681ec2592fc8085a6528822480f335971d86479
AndersonAndrad <anderson_andrade_@outlook.com>
2023-11-16 00:54:44 -0300

    feature: implements a service to read all routes and expose the application api for use in others services

--- src/serve.js
+++ src/serve.js
@@ -1,8 +1,33 @@
-const express = require('express')
-const app = new express()
+import express from 'express'
+import { DynamicsRoutes } from './dynamics.router'
 
-app.get('/', (req, res) => {
-  res.send('hello word')
-})
+class Serve {
+  constructor () {
+    this.expressInstance = express()
 
-app.listen(3000)
+    this.dynamicsRoutes = new DynamicsRoutes()
+
+    this.setRoutes()
+  }
+
+  /**
+   * attach all dynamics routes to the server
+   */
+  setRoutes () {
+    this.dynamicsRoutes.setupRoutes()
+    this.dynamicsRoutes.attachToApp(this.expressInstance)
+  }
+
+  /**
+   * expose the server on the port specifies in the environments variables or default port 3000
+   */
+  createServer () {
+    const serverPort = process.env.APP_PORT || 3000
+    this.expressInstance.listen(serverPort, () => {
+      console.log(`Server is running on port ${serverPort}`)
+    })
+  }
+}
+
+const server = new Serve()
+server.createServer()
