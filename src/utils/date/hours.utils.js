4d5b0f00dbb2cd0c4f29610ecf680dd73d71895f
AndersonAndrad <anderson_andrade_@outlook.com>
2024-01-14 23:01:42 -0300

    feat: create date utils to return current hours in format hour:minute: second PM/AM

---
+++ src/utils/date/hours.utils.js
@@ -0,0 +1,17 @@
+export const getCurrentTime = () => {
+  const currentDate = new Date()
+
+  let hours = currentDate.getHours()
+  let minutes = currentDate.getMinutes()
+  let seconds = currentDate.getSeconds()
+
+  hours = (hours < 10) ? '0' + hours : hours
+  minutes = (minutes < 10) ? '0' + minutes : minutes
+  seconds = (seconds < 10) ? '0' + seconds : seconds
+
+  const hours24 = (hours >= 12) ? 'PM' : 'AM'
+
+  hours = (hours === 0) ? 12 : hours
+
+  return `${hours}:${minutes}:${seconds} ${hours24}`
+}
