8e6785cc9358edb6525821bcc5229ad7f605d9c8
Ygor Fortes <ygor1888@hotmail.com>
2024-01-15 17:37:53 -0300

    chore: Imported and instantiated the json method from express. Used in router.use. Instantiated handleRequestErrors and handleErro404 methods from error.middlewares.

--- src/dynamics.router.js
+++ src/dynamics.router.js
@@ -1,22 +1,31 @@
-import { Router } from 'express'
+import { Router, json } from 'express'
 import { AppController } from './modules/app/app.controller.js'
 import { UserController } from './modules/user/user.controller.js'
+import { AuthController } from './modules/auth/auth.controller.js'
+import { ErrorMiddlewares } from './middlewares/error.middlewares.js'
 
 export class DynamicsRoutes {
   constructor () {
     this.router = Router()
+    this.json = json()
   }
 
   /**
    * integrate routes in the service based on controllers
    */
   setupRoutes () {
+    this.router.use(this.json)
     const appController = new AppController()
     const userController = new UserController()
+    const authController = new AuthController()
+    const errorMiddlewares = new ErrorMiddlewares()
 
     this.router.use('/', appController.routes())
-
+    this.router.use('/', authController.routes())
     this.router.use('/user', userController.routes())
+
+    this.router.use(errorMiddlewares.handleRequestErrors())
+    this.router.use(errorMiddlewares.handleErro404())
   }
 
   /**
