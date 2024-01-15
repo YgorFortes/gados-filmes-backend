da3298a35fb317ef30afaffd20bc9cd3bcf92cea
Ygor Fortes <ygor1888@hotmail.com>
2024-01-15 17:44:26 -0300

    feat(auth.validator.schema): Created authentication validator class with a method to validate login.

---
+++ src/modules/auth/validators/auth.validator.schema.js
@@ -0,0 +1,13 @@
+import { object, string } from 'yup'
+
+export class AuthValidatorSchema {
+  async login (value) {
+    const authSchema = object({
+      login: string().trim().matches(/^[^\s]+$/, 'O campo login não pode conter espaços entre os caracteres.'),
+      email: string().email('O campo precisa ser um email válido.').trim().lowercase(),
+      senha: string().trim().required('O campo senha é obrigatório.').matches(/^[^\s]+$/, 'O campo senha não pode conter espaços entre os caracteres.')
+    }).test('emailOrLogin', 'Deve fornecer email ou login.', (body) => Boolean(body.login || body.email)).noUnknown()
+
+    return await authSchema.validate(value)
+  };
+}
