0d3bbf5c0ed03ffb8cd80da6a742eb7b788aa9fc
Vitor <vitor95ramos@gmail.com>
2024-01-21 12:17:57 -0300

    feat: criado o  validador de elements do body para cadastro de um usuário

--- src/modules/user/validators/validator.schema.js
+++ src/modules/user/validators/validator.schema.js
@@ -1,11 +1,13 @@
 import { object, string } from 'yup'
 
-export class ValidatorSchema {
-  async findAll (value) {
-    const userSchema = object({
-      name: string().required()
-    })
-
-    return await userSchema.validate({})
+export class UserValidatorSchema {
+  async registerUser (value) {
+    const RegisterUserValidatorSchema = object({
+      login: string().trim().matches(/^[^\s]+$/, 'O campo login não pode conter espaços entre os caracteres.').required('O campo login é obrigatório'),
+      email: string().email('O campo precisa ser um email válido.').trim().lowercase().required('O campo email obrigatório.'),
+      senha: string().trim().required('O campo senha é obrigatório.').matches(/^[^\s]+$/, 'O campo senha não pode conter espaços entre os caracteres.'),
+      nome: string().trim().required('O campo nome é obrigatório.')
+    }).noUnknown()
+    return await RegisterUserValidatorSchema.validate(value)
   }
 }
