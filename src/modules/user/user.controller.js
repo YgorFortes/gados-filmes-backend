ccd3e625aed4bfb00d9dafe1e2c5651135a5424f
Vitor <vitor95ramos@gmail.com>
2024-01-21 12:21:35 -0300

    feat: created controller to register a user

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,5 +1,5 @@
 import { UserService } from './services/user.service.js'
-import { ValidatorSchema } from './validators/validator.schema.js'
+import { UserValidatorSchema } from './validators/validator.schema.js'
 import { CrudControllerUtils } from '../../utils/crud/crud-controller.utils.js'
 
 export class UserController extends CrudControllerUtils {
@@ -8,14 +8,17 @@ export class UserController extends CrudControllerUtils {
 
     this.userService = new UserService()
 
-    this.validators = new ValidatorSchema()
+    this.validators = new UserValidatorSchema()
   }
 
   create () {
-    this.router.get('/', async (req, res) => {
-      const users = await this.userService.findAll()
-
-      return res.status(200).send(users)
+    this.router.post('/cadastrarusuario', async (req, res, next) => {
+      try {
+        const userBodyValidated = await this.validators.registerUser(req.body)
+        await this.userService.createUser({ ...userBodyValidated })
+        return res.status(200).json({ mensagem: 'Usuário cadastrado com sucesso' })
+      } catch (error) {
+      }
     })
   }
 }
