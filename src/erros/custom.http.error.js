aa132c2c204ab5661c06888b4e33445fccb67ba1
Vitor <vitor95ramos@gmail.com>
2024-01-21 12:11:05 -0300

    chore: The errors folder was created and the custom.http.error.js file was created inside the folder. The CustomHttpError class was created within the file to deal with https errors

---
+++ src/erros/custom.http.error.js
@@ -0,0 +1,15 @@
+export class CustomHttpError extends Error {
+  constructor (message = 'Problemas no servidor! Volte mais tarde', statusCode = 500) {
+    super(message)
+    this.message = message
+    this.statusCode = statusCode
+  }
+
+  static checkAndThrowError (error) {
+    if (error instanceof CustomHttpError) {
+      throw error
+    }
+
+    throw new Error()
+  }
+}
