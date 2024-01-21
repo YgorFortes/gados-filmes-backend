1dcb46f11dee866483d18b34fb157684e8c0020c
Vitor <vitor95ramos@gmail.com>
2024-01-21 12:25:30 -0300

    feat: created the service to register a user. The method responsible for this action is createUser

--- src/modules/user/services/user.service.js
+++ src/modules/user/services/user.service.js
@@ -1,17 +1,37 @@
 import { UserRepository } from '../repository/user.repository.js'
 import { CrudServiceUtils } from '../../../utils/crud/crud-service.utils.js'
+import { UserValidatorSchema } from '../validators/validator.schema.js'
+import { CustomHttpError } from '../../../erros/custom.http.error.js'
+import { UtilsBcrypt } from '../utils/bcrypt.js'
 
 export class UserService extends CrudServiceUtils {
   constructor () {
     super()
     this.userRepository = new UserRepository()
+    this.userValidatorSchema = new UserValidatorSchema()
   }
 
   async findAll () {
     return this.userRepository.findAll()
   }
 
-  async findOne () {
-    return {}
+  async createUser (dataUse) {
+    try {
+      const { senha, ...restObject } = dataUse
+      const hash = await UtilsBcrypt.hashPassword(senha)
+      console.log(hash)
+      console.log(restObject)
+      const newUserIfHash = {
+        nome: restObject.nome,
+        login: restObject.login,
+        email: restObject.email,
+        senha: hash
+      }
+      const userValidated = await this.userValidatorSchema.registerUser(newUserIfHash)
+      const newUserCreated = this.userRepository.createUser(userValidated)
+      return newUserCreated
+    } catch (error) {
+      CustomHttpError.checkAndThrowError(error)
+    }
   }
 }
