6aaa47f26790034cb01d20ac169c10963ed795fa
Vitor <vitor95ramos@gmail.com>
2024-01-21 12:08:33 -0300

    feat: created the user registration route and configured it to receive body data as json

--- src/dynamics.router.js
+++ src/dynamics.router.js
@@ -1,22 +1,30 @@
-import { Router } from 'express'
+import { Router, json } from 'express'
 import { AppController } from './modules/app/app.controller.js'
 import { UserController } from './modules/user/user.controller.js'
+import { CheckUserExist } from './middlewares/check.user.exist.js'
+import { ErrorMiddlewares } from './middlewares/error.midlewares.js'
 
 export class DynamicsRoutes {
   constructor () {
     this.router = Router()
+    this.json = json()
   }
 
   /**
    * integrate routes in the service based on controllers
    */
   setupRoutes () {
+    this.router.use(this.json)
     const appController = new AppController()
     const userController = new UserController()
+    const checkUserExist = new CheckUserExist()
+    const errorMiddlewares = new ErrorMiddlewares()
 
     this.router.use('/', appController.routes())
+    this.router.use('/', checkUserExist.findUser(), userController.routes())
 
-    this.router.use('/user', userController.routes())
+    this.router.use(errorMiddlewares.handleRequestErrors())
+    this.router.use(errorMiddlewares.handleErro404())
   }
 
   /**
