7bea06087a3dadae689eef6d8af1d61b201e361c
Vitor <vitor95ramos@gmail.com>
2024-01-23 22:01:40 -0300

    chore: created the movie module repository file

---
+++ src/modules/movie/repository/movie.repository.js
@@ -0,0 +1,29 @@
+import { CrudRepositoryUtils } from '../../../utils/crud/crud-repository.utils.js'
+
+export class UserRepository extends CrudRepositoryUtils {
+  findAll () {
+    return this.prismaClient.user.findMany()
+  }
+
+  findByLoginOrEmail (login, email) {
+    return this.prismaClient.user.findFirst({
+      where: {
+        OR: [
+          { login },
+          { email }
+        ]
+      }
+    })
+  }
+
+  createUser (dataUse) {
+    return this.prismaClient.user.create({
+      data: {
+        login: dataUse.login,
+        email: dataUse.email,
+        nome: dataUse.nome,
+        senha: dataUse.senha
+      }
+    })
+  }
+}
