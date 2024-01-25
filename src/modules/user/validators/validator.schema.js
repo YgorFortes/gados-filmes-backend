2b0172ac1d2eca5b53c79c557b1d171ce46985af
Vitor <vitor95ramos@gmail.com>
2024-01-24 23:29:16 -0300

    fix: changed the registerUser method name to validateUserToRegistre, so that it remains semantic

--- src/modules/user/validators/validator.schema.js
+++ src/modules/user/validators/validator.schema.js
@@ -1,7 +1,7 @@
 import { object, string } from 'yup'
 
-export class UserValidatorSchema {
-  async registerUser (value) {
+export class ValidateUserSchema {
+  async validateUserToRegister (value) {
     const RegisterUserValidatorSchema = object({
       login: string().trim().matches(/^[^\s]+$/, 'O campo login não pode conter espaços entre os caracteres.').required('O campo login é obrigatório'),
       email: string().email('O campo precisa ser um email válido.').trim().lowercase().required('O campo email obrigatório.'),
