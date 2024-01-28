bd102d358d05d2016c58854c400a0d8823dcdb7e
Vitor <vitor95ramos@gmail.com>
2024-01-28 18:59:33 -0300

    chore: Created the method to locate a film

--- src/modules/movie/movie.controller.js
+++ src/modules/movie/movie.controller.js
@@ -1,20 +1,25 @@
-import { CustomHttpError } from '../../erros/custom.http.error.js'
 import { Logger } from '../../infra/logger/logger.service.js'
 import { CrudControllerUtils } from '../../utils/crud/crud-controller.utils.js'
+import { MovieService } from './services/movie.service.js'
+import { MovieValidatorSchema } from './validators/validator.schema.js'
 
 export class MovieController extends CrudControllerUtils {
   constructor () {
     super()
+    this.movieService = new MovieService()
     this.logger = new Logger(MovieController.name)
+    this.validators = new MovieValidatorSchema()
   }
 
   findOne () {
-    this.router.get('/filmes', async (req, res, next) => {
+    this.router.get('/filmes/titulo/:titulo', async (req, res, next) => {
       try {
-        res.status(200).send('Chegou na rota de pesquisar filme')
+        const titleValidated = await this.validators.validateTitleTofindMovie(req.params)
+        const movie = await this.movieService.findMovie(titleValidated.titulo)
+        res.status(200).send(movie)
       } catch (error) {
-        this.logger.dispatch('error', `Erro durante a validação da senha: ${error.message}`)
-        CustomHttpError.checkAndThrowError(error)
+        next(error)
+        this.logger.dispatch('debug', error.message)
       }
     })
   }
