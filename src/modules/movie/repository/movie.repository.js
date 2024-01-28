7dc2da2f8ee978ce7a5be94bb1705de64de204ac
Vitor <vitor95ramos@gmail.com>
2024-01-28 19:01:01 -0300

    chore: created the data using findMovie and createMovie methods

--- src/modules/movie/repository/movie.repository.js
+++ src/modules/movie/repository/movie.repository.js
@@ -1,28 +1,38 @@
 import { CrudRepositoryUtils } from '../../../utils/crud/crud-repository.utils.js'
 
-export class UserRepository extends CrudRepositoryUtils {
-  findAll () {
-    return this.prismaClient.user.findMany()
-  }
-
-  findByLoginOrEmail (login, email) {
-    return this.prismaClient.user.findFirst({
+export class MovieRepository extends CrudRepositoryUtils {
+  findMovie (titulo) {
+    return this.prismaClient.movie.findFirst({
       where: {
-        OR: [
-          { login },
-          { email }
-        ]
+        title: {
+          contains: titulo,
+          mode: 'insensitive'
+        }
       }
     })
   }
 
-  createUser (dataUse) {
-    return this.prismaClient.user.create({
+  createMovie (dataMovie) {
+    return this.prismaClient.movie.create({
       data: {
-        login: dataUse.login,
-        email: dataUse.email,
-        nome: dataUse.nome,
-        senha: dataUse.senha
+        imdb_id: dataMovie.imdb_id,
+        title: dataMovie.title,
+        rated: dataMovie.rated,
+        released: dataMovie.released,
+        runtime: dataMovie.runtime,
+        genre: dataMovie.genre,
+        director: dataMovie.director,
+        writer: dataMovie.writer,
+        actors: dataMovie.actors,
+        plot: dataMovie.plot,
+        language: dataMovie.language,
+        country: dataMovie.country,
+        awards: dataMovie.awards,
+        poster: dataMovie.poster,
+        metascore: dataMovie.metascore,
+        imdb_rating: dataMovie.imdb_rating,
+        imdb_votes: dataMovie.imdb_votes,
+        type: dataMovie.type
       }
     })
   }
