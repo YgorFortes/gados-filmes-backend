022f538ded252afd6dad0ade4562af61ab11f0ee
Vitor <vitor95ramos@gmail.com>
2024-01-28 19:08:17 -0300

    The omdb service was created. In this case, if the film is not in the database, a call will be made to the OMDB API

---
+++ src/modules/movie/services/omdb.service.js
@@ -0,0 +1,51 @@
+import { MovieRepository } from '../repository/movie.repository.js'
+import { CrudServiceUtils } from '../../../utils/crud/crud-service.utils.js'
+import { CustomHttpError } from '../../../erros/custom.http.error.js'
+import { Logger } from '../../../infra/logger/logger.service.js'
+import axios from 'axios'
+import { FormatDate } from '../utils/format.date.js'
+
+export class OmdbService extends CrudServiceUtils {
+  constructor () {
+    super()
+    this.movieRepository = new MovieRepository()
+    this.logger = new Logger(OmdbService.name)
+  }
+
+  async createMovieOmdb (titulo) {
+    console.log('chamou o createMovieIMDB')
+    try {
+      const url = `https://www.omdbapi.com/?t=${titulo}&apikey=${process.env.KEY_OMDB}`
+      const response = await axios.get(url)
+      const dataMovie = response.data
+      const convertBolleanResponse = response.data.Response.toLowerCase() === 'true'
+      if (!convertBolleanResponse) {
+        return convertBolleanResponse
+      }
+
+      const movieWithDatesFormat = {
+        imdb_id: dataMovie.imdbID,
+        title: dataMovie.Title,
+        rated: dataMovie.Rated,
+        released: await FormatDate.formatDateToBbStandard(dataMovie.Released),
+        runtime: dataMovie.Runtime,
+        genre: dataMovie.Genre,
+        director: dataMovie.Director,
+        writer: dataMovie.Writer,
+        actors: dataMovie.Actors,
+        plot: dataMovie.Plot,
+        language: dataMovie.Language,
+        country: dataMovie.Country,
+        awards: dataMovie.Awards,
+        poster: dataMovie.Poster,
+        metascore: dataMovie.Metascore,
+        imdb_rating: dataMovie.imdbRating,
+        imdb_votes: dataMovie.imdbVotes,
+        type: dataMovie.Type
+      }
+      return await this.movieRepository.createMovie(movieWithDatesFormat)
+    } catch (error) {
+      CustomHttpError.checkAndThrowError(error)
+    }
+  }
+}
