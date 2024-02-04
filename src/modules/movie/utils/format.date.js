cad477e6511067722544e37a0896c841b65eb9a4
Vitor <vitor95ramos@gmail.com>
2024-02-04 19:20:39 -0300

    fix: removed async from formatDateToBbStandard method

--- src/modules/movie/utils/format.date.js
+++ src/modules/movie/utils/format.date.js
@@ -1,7 +1,7 @@
 import { parse, format } from 'date-fns'
 
 export class FormatDate {
-  static async formatDateToBbStandard (date) {
+  static formatDateToBbStandard (date) {
     const dataFormatada = format(parse(date, 'dd MMM yyyy', new Date()), 'yyyy-MM-dd')
     return new Date(dataFormatada)
   }
