f70ff0ae9190b2cb575d2332668997b6c598b8be
Ygor Fortes <ygor1888@hotmail.com>
2024-02-05 17:18:57 -0300

    feat: created new endpoint GET /home where search top 10 movies rated from users or based in pa
    created new method in homeController called home where retorn top movies ratings users

--- src/modules/movie/home.controller.js
+++ src/modules/movie/home.controller.js
@@ -1,23 +1,27 @@
-import { MovieService } from './services/movie.services.js'
-import { CrudControllerUtils } from '../../utils/crud/crud-controller.utils.js'
+import { CrudControllerUtils } from '../../utils/crud/crud-controller.utils.js';
+import { MovieService } from './services/movie.services.js';
+import { ValidateMovieSchema } from './validators/movieSchema.validator.js';
 
 export class HomeController extends CrudControllerUtils {
   constructor () {
-    super()
+    super();
 
-    this.movieService = new MovieService()
-    this.setupRouter(this.home())
+    this.movieService = new MovieService();
+    this.validateMovieSchema = new ValidateMovieSchema();
+    this.setupRouter(this.home());
   }
 
   async home () {
     this.router.get('/home', async (req, res, next) => {
       try {
-        const topMoviesRatingsUsers = await this.movieService.findTopRatedMoviesByUsers()
+        const filter = await this.validateMovieSchema.validateRatedMovieFilter(req.query);
 
-        return res.status(200).send(topMoviesRatingsUsers)
+        const topMoviesRatingsUsers = await this.movieService.findTopRatedMoviesByUsers({ ...filter });
+
+        return res.status(200).send(topMoviesRatingsUsers);
       } catch (error) {
-        next(error)
+        next(error);
       }
-    })
+    });
   }
 }
