c82de389f339372f5af97d363d51905f40a3e060
Vitor <vitor95ramos@gmail.com>
2024-02-06 21:34:56 -0300

    fix: Adding the semicolons and correcting the class path

--- src/modules/movie/services/movie.service.js
+++ src/modules/movie/services/movie.service.js
@@ -1,32 +1,32 @@
-import { MovieRepository } from '../repository/movie.repository.js'
-import { CrudServiceUtils } from '../../../utils/crud/crud-service.utils.js'
-import { CustomHttpError } from '../../../erros/custom.http.error.js'
-import { Logger } from '../../../infra/logger/logger.service.js'
-import { OmdbService } from './omdb.service.js'
+import { MovieRepository } from '../repository/movie.repository.js';
+import { CrudServiceUtils } from '../../../utils/crud/crud-service.utils.js';
+import { Logger } from '../../../infra/logger/logger.service.js';
+import { OmdbService } from './omdb.service.js';
+import { CustomHttpError } from '../../../erros/customHttp.error.js';
 
 export class MovieService extends CrudServiceUtils {
   constructor () {
-    super()
-    this.movieRepository = new MovieRepository()
-    this.logger = new Logger(MovieService.name)
-    this.omdbService = new OmdbService()
+    super();
+    this.movieRepository = new MovieRepository();
+    this.logger = new Logger(MovieService.name);
+    this.omdbService = new OmdbService();
   }
 
   async findMovie (titulo) {
     try {
-      const movie = await this.movieRepository.findMovie(titulo)
+      const movie = await this.movieRepository.findMovie(titulo);
       if (!movie) {
-        const responseOmdbCreateMovie = await this.omdbService.createMovieOmdb(titulo)
+        const responseOmdbCreateMovie = await this.omdbService.createMovieOmdb(titulo);
         if (!responseOmdbCreateMovie) {
-          const mensagem = { mensagem: 'Filme não encontrado.' }
-          return mensagem
+          const mensagem = { mensagem: 'Filme não encontrado.' };
+          return mensagem;
         }
-        const movie = await this.movieRepository.findMovie(titulo)
-        return movie
+        const movie = await this.movieRepository.findMovie(titulo);
+        return movie;
       }
-      return movie
+      return movie;
     } catch (error) {
-      CustomHttpError.checkAndThrowError(error)
+      CustomHttpError.checkAndThrowError(error);
     }
   }
 }
--- src/modules/movie/services/omdb.service.js
+++ src/modules/movie/services/omdb.service.js
@@ -1,25 +1,26 @@
-import { MovieRepository } from '../repository/movie.repository.js'
-import { CrudServiceUtils } from '../../../utils/crud/crud-service.utils.js'
-import { CustomHttpError } from '../../../erros/custom.http.error.js'
-import { Logger } from '../../../infra/logger/logger.service.js'
-import axios from 'axios'
-import { FormatDate } from '../utils/format.date.js'
+import { CrudServiceUtils } from '../../../utils/crud/crud-service.utils.js';
+import { MovieRepository } from '../repository/movie.repository.js';
+
+import axios from 'axios';
+import { CustomHttpError } from '../../../erros/customHttp.error.js';
+import { Logger } from '../../../infra/logger/logger.service.js';
+import { FormatDate } from '../utils/format.date.js';
 
 export class OmdbService extends CrudServiceUtils {
   constructor () {
-    super()
-    this.movieRepository = new MovieRepository()
-    this.logger = new Logger(OmdbService.name)
+    super();
+    this.movieRepository = new MovieRepository();
+    this.logger = new Logger(OmdbService.name);
   }
 
   async createMovieOmdb (titulo) {
     try {
-      const url = `https://www.omdbapi.com/?t=${titulo}&apikey=${process.env.KEY_OMDB}`
-      const response = await axios.get(url)
-      const dataMovie = response.data
-      const convertBolleanResponse = response.data.Response.toLowerCase() === 'true'
+      const url = `https://www.omdbapi.com/?t=${titulo}&apikey=${process.env.KEY_OMDB}`;
+      const response = await axios.get(url);
+      const dataMovie = response.data;
+      const convertBolleanResponse = response.data.Response.toLowerCase() === 'true';
       if (!convertBolleanResponse) {
-        return convertBolleanResponse
+        return convertBolleanResponse;
       }
 
       const movieWithDatesFormat = {
@@ -41,10 +42,10 @@ export class OmdbService extends CrudServiceUtils {
         imdb_rating: dataMovie.imdbRating,
         imdb_votes: dataMovie.imdbVotes,
         type: dataMovie.Type
-      }
-      return await this.movieRepository.createMovie(movieWithDatesFormat)
+      };
+      return await this.movieRepository.createMovie(movieWithDatesFormat);
     } catch (error) {
-      CustomHttpError.checkAndThrowError(error)
+      CustomHttpError.checkAndThrowError(error);
     }
   }
 }
