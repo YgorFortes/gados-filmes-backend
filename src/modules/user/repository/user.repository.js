4e45f9110124daf2c09ef64b429ba17f8f3d81f8
Ygor Fortes <ygor1888@hotmail.com>
2024-02-08 08:05:48 -0300

    feat: now is capable find user for id, associate movie with user and find this association
    created method findOne, addMovieToUser and findMovieUserAossication in user.repository

--- src/modules/user/repository/user.repository.js
+++ src/modules/user/repository/user.repository.js
@@ -16,6 +16,12 @@ export class UserRepository extends CrudRepositoryUtils {
     });
   }
 
+  findOne (idUser) {
+    return this.prismaClient.user.findUnique({
+      where: { id: idUser }
+    });
+  }
+
   createUser (dataUse) {
     return this.prismaClient.user.create({
       data: {
@@ -23,6 +29,23 @@ export class UserRepository extends CrudRepositoryUtils {
         email: dataUse.email,
         nome: dataUse.nome,
         senha: dataUse.senha
+      }
+    });
+  }
+
+  addMovieToUser (movieUserData, idUser) {
+    const { idFilme, classificacao = null } = movieUserData ?? {};
+
+    return this.prismaClient.movies_users.create({
+      data: { idfilmes: idFilme, classificacao, idusuarios: idUser }
+    });
+  }
+
+  findMovieUserAssociation (idMovie, idUser) {
+    return this.prismaClient.movies_users.findFirst({
+      where: {
+        idfilmes: idMovie,
+        idusuarios: idUser
       }
     });
   }
