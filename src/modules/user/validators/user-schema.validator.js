632b2a55ba7fc6aa4c6f23ffbdad15ef6a46a4d9
Ygor Fortes <ygor1888@hotmail.com>
2024-02-19 18:19:41 -0300

    feat validated request body and params for rateMovieByUser in user.controller
    created method rateMovieByUser in user-schema.validator
    
    chore: renamed validateIdMovie parameter for value insted reqBody

--- src/modules/user/validators/user-schema.validator.js
+++ src/modules/user/validators/user-schema.validator.js
@@ -1,4 +1,4 @@
-import { object, string, number } from 'yup';
+import { number, object, string } from 'yup';
 
 export class ValidateUserSchema {
   async validateUserToRegister (value) {
@@ -34,10 +34,22 @@ export class ValidateUserSchema {
     return await ValidateQueryPagination.validate(value);
   }
 
-  async validateIdMovie (reqBody) {
+  async validateIdMovie (value) {
     const validateIdFilmes = object({
       idfilmes: number().typeError('O campo classificacao só recebe números.').integer('O campo classificacao só recebe números interiros.').positive('O campo page só recebe números positivos').required('O campo idfilmes é obrigatório.')
     }).noUnknown();
-    return await validateIdFilmes.validate(reqBody);
+    return await validateIdFilmes.validate(value);
+  }
+
+  async rateMovieByUser (reqBody, reqParams) {
+    const moviePatchSchameValidate = object({
+      classificacao: number().typeError('O campo classificacao só recebe números.').integer('O campo classificacao só recebe números interiros.')
+        .positive('O campo page só recebe números positivos.')
+        .max(10, 'O campo classificacao deve ser no máximo 10.').min(1, 'O campo classificacao deve ser no mínimo 1.').required('O campo classificacao é obrigátorio')
+    });
+    const classificacao = await moviePatchSchameValidate.validate(reqBody);
+    const idFilme = (await this.validateIdMovie(reqParams)).idfilmes;
+
+    return { ...classificacao, idFilme };
   }
 }
