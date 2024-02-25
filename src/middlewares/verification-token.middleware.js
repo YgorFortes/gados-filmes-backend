3a11179c4b8560eaf70f488eccd41c62820e03b5
Ygor Fortes <ygor1888@hotmail.com>
2024-02-25 11:18:40 -0300

    feat: create one list of token invalids and verify in middlewares if token is invalid, after one week this token invalid is deleted from system
    create new middleware called removed token, and create methods called verifyTokenInvalid and deleteTokenInOneWeek

--- src/middlewares/verification-token.middleware.js
+++ src/middlewares/verification-token.middleware.js
@@ -1,10 +1,15 @@
 import jsonwebtoken from 'jsonwebtoken';
+import nodeCrom from 'node-cron';
 import { CustomHttpError } from '../erros/custom-http.error.js';
 import { Logger } from '../infra/logger/logger.service.js';
 
 export class VerificationTokenMiddleware {
+  static tokenInvalids = [];
   constructor () {
     this.logger = new Logger(VerificationTokenMiddleware.name);
+    nodeCrom.schedule('0 9 * * 1', () => {
+      this.deletokenInOneWeek();
+    });
   }
 
   checkAuthToken () {
@@ -24,6 +29,7 @@ export class VerificationTokenMiddleware {
       }
 
       try {
+        this.verifyTokenInvalid(token);
         const secretKey = process.env.SECRET;
 
         jsonwebtoken.verify(token, secretKey);
@@ -36,5 +42,44 @@ export class VerificationTokenMiddleware {
         throw new CustomHttpError('Token inválido', 401);
       }
     };
+  }
+
+  static removeToken () {
+    return (req, _, next) => {
+      try {
+        const tokenHeaders = req.headers.authorization;
+
+        if (!tokenHeaders) {
+          throw new CustomHttpError('Token não existe. Certifique-se de incluir o token no cabeçalho Authorization.', 401);
+        }
+
+        // eslint-disable-next-line no-unused-vars
+        const [_, token] = tokenHeaders && tokenHeaders.split(' ');
+
+        VerificationTokenMiddleware.tokenInvalids.push(token);
+
+        next();
+      } catch (error) {
+        throw new CustomHttpError('Token inválido', 401);
+      }
+    };
+  }
+
+  verifyTokenInvalid (token) {
+    try {
+      if (VerificationTokenMiddleware.tokenInvalids.includes(token)) {
+        throw CustomHttpError('Token Invalido', 401);
+      }
+    } catch (error) {
+      CustomHttpError.checkAndThrowError(error);
+    }
+  }
+
+  deletokenInOneWeek () {
+    try {
+      this.tokenInvalids.splice(0, VerificationTokenMiddleware.tokenInvalids.length);
+    } catch (error) {
+      CustomHttpError.checkAndThrowError(error);
+    }
   }
 }
